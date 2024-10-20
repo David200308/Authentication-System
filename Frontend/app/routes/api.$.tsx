7d97bf900f4cb455f83fc084e25cc917f78fd204
David Jiang <jiangguanlin2003@outlook.com>
2024-10-20 08:36:25 +0800

    fix: api middleware problem

--- Frontend/app/routes/api.$.tsx
+++ Frontend/app/routes/api.$.tsx
@@ -3,14 +3,32 @@ import type { LoaderFunction, ActionFunction } from "@remix-run/node";
 const ORIGIN = import.meta.env.VITE_PUBLIC_BACKEND_URL;
 
 const proxyRequest = (request: Request) => {
-    const { pathname } = new URL(request.url.split("/api").join(""));
-    const { href } = new URL(pathname, ORIGIN);
-
-    return new Request(href, request);
+  const { pathname } = new URL(request.url.split("/api").join(""));
+  const { href } = new URL(pathname, ORIGIN);
+  
+  const headers = new Headers(request.headers);
+  
+  headers.set('Host', ORIGIN.split('//')[1]);
+  
+  return new Request(href, {
+    method: request.method,
+    headers: headers,
+    body: request.body,
+    mode: request.mode,
+    credentials: request.credentials,
+    cache: request.cache,
+    redirect: request.redirect,
+    referrer: request.referrer,
+    integrity: request.integrity,
+  });
 };
 
-export const loader: LoaderFunction = async ({ request }) =>
-    fetch(proxyRequest(request));
+export const loader: LoaderFunction = async ({ request }) => {
+  const proxyReq = proxyRequest(request);
+  return fetch(proxyReq);
+};
 
-export const action: ActionFunction = ({ request }) =>
-    fetch(proxyRequest(request));
+export const action: ActionFunction = async ({ request }) => {
+  const proxyReq = proxyRequest(request);
+  return fetch(proxyReq);
+};
