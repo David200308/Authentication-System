1b3dc63c72ea919e807d6a3fe67d89e5b5081ba8
David Jiang <jiangguanlin2003@outlook.com>
2024-10-29 23:49:01 +0800

    fix: frontend email verify page

--- Frontend/app/routes/verify-email/$email.$token.tsx
+++ Frontend/app/routes/verify-email/$email.$token.tsx
@@ -12,7 +12,7 @@ interface VerifyResponse {
     status?: boolean;
 }
 
-async function verify2FA(data: VerifyEmailData): Promise<VerifyResponse> {
+async function verifyEmail(data: VerifyEmailData): Promise<VerifyResponse> {
     const response = await fetch("/api/user/activate", {
         method: "POST",
         headers: {
@@ -48,15 +48,15 @@ async function verifyToken() {
 
 export default function VerifyEmail() {
     const navigate = useNavigate();
-    const params = useParams();
+    const { email, token } = useParams();
 
-    const [formData, ] = useState<VerifyEmailData>({
-        email: params?.email || "",
-        token: params?.token || "",
+    const [formData,] = useState<VerifyEmailData>({
+        email: email || "",
+        token: token || "",
     });
 
     const verifyEmailMutation = useMutation<VerifyResponse, Error, VerifyEmailData>({
-        mutationFn: verify2FA,
+        mutationFn: verifyEmail,
         onSuccess: (data: VerifyResponse) => {
             console.log("User logged in successfully:", data);
             if (data.status) {
@@ -92,27 +92,36 @@ export default function VerifyEmail() {
     return (
         <div className="flex h-screen items-center justify-center bg-gray-100">
             <div className="text-center">
-                <h1 className="text-4xl font-bold mb-8 text-black">
-                    Verify Email
-                </h1>
-                <Form method="post" className="space-y-4" onSubmit={handleSubmit}>
+                {email && token && (
                     <div>
-                        <input
-                            type="text"
-                            name="email"
-                            className="px-4 py-2 border rounded w-80 mb-2 bg-white text-black focus:outline-none focus:ring focus:border-blue-300"
-                            value={formData.email}
-                            disabled
-                        />
+                        <h1 className="text-4xl font-bold mb-8 text-black">
+                            Verify Email
+                        </h1>
+                        <Form method="post" className="space-y-4" onSubmit={handleSubmit}>
+                            <div>
+                                <input
+                                    type="text"
+                                    name="email"
+                                    className="px-4 py-2 border rounded w-80 mb-2 bg-white text-black focus:outline-none focus:ring focus:border-blue-300"
+                                    value={formData.email}
+                                    disabled
+                                />
+                            </div>
+
+                            {verifyEmailMutation.isPending && <p>Verifying in...</p>}
+                            {verifyEmailMutation.isError && <p className="text-red-500">Error: {verifyEmailMutation.error.message}</p>}
+
+                            <button className="px-6 py-2 w-full bg-black text-white rounded hover:bg-gray-800" type="submit" disabled={verifyEmailMutation.isPending}>
+                                Activate
+                            </button>
+                        </Form>
                     </div>
-
-                    {verifyEmailMutation.isPending && <p>Verifying in...</p>}
-                    {verifyEmailMutation.isError && <p className="text-red-500">Error: {verifyEmailMutation.error.message}</p>}
-
-                    <button className="px-6 py-2 w-full bg-black text-white rounded hover:bg-gray-800" type="submit" disabled={verifyEmailMutation.isPending}>
-                        Activate
-                    </button>
-                </Form>
+                )}
+                {!email || !token && (
+                    <h1 className="text-4xl font-bold mb-8 text-black">
+                        Please check your email for the verification link.
+                    </h1>
+                )}
             </div>
         </div>
     );
