d74f3459b16f5e8e5ce039a682bcac274b4aacf4
David Jiang <jiangguanlin2003@outlook.com>
2024-11-17 10:31:22 +0800

    feat: add redis cache db integration

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 import { UserController } from './controllers/user';
 import { UserServices } from './services/user';
 import { ConfigModule } from '@nestjs/config';
+import { RedisOptions } from './redis.config.options';
 import { SentryModule } from '@sentry/nestjs/setup';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -10,8 +13,16 @@ import { SentryModule } from '@sentry/nestjs/setup';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
-    })],
+    }),
+    CacheModule.registerAsync(RedisOptions),
+  ],
   controllers: [UserController],
-  providers: [UserServices],
+  providers: [
+    UserServices,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule { }
